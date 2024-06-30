ca0949ee75b488c8a264fd76df2450d108f40543
afifahasnianr <afifahasniaar@gmail.com>
2024-06-30 18:43:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -99,4 +99,101 @@ if selected == "Home":
         new_title = f'<p style="font-family:Georgia; color: white; font-size: 34px;">{member}</p>'
         st.markdown(new_title, unsafe_allow_html=True)
 
-   
+# Main application
+def main():
+    st.title('Final Project ECG ASN')
+
+    # Sidebar menu
+    selected = st.sidebar.selectbox("Select Page", ["Home", "ECG Detection"])
+
+    figs = []  # Initialize figs here to ensure it's accessible
+
+    if selected == "Home":
+        st.subheader("Anggota kelompok")
+        
+        # Display images
+        image1 = Image.open("fotofutull.jpg")
+        image2 = Image.open("fotoflo.jpg")
+        image3 = Image.open("fotovas.jpg")
+        
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.image(image1, caption="Nadhifatul Fuadah - 5023211053", width=150)
+        with col2:
+            st.image(image2, caption="Florencia Irena - 5023211009", width=150)
+        with col3:
+            st.image(image3, caption="Vasya Maharani Putri - 5023211033", width=150)
+
+    elif selected == "ECG Detection":
+        st.subheader('ECG Detection')
+
+        # Upload file
+        uploaded_file = st.file_uploader("Upload file XLSX untuk data ECG", type=["xlsx"])
+
+        if uploaded_file is not None:
+            # Process ECG data
+            fig, data_a, fig_hn, fig_gn, Hw, Gw = process_ecg_data(uploaded_file)
+
+            if fig is not None:
+                # Display ECG plot
+                st.plotly_chart(fig)
+
+                # Display h(n) and g(n) plots
+                st.subheader('Impuls Respon')
+                st.plotly_chart(fig_hn)
+                st.plotly_chart(fig_gn)
+
+                # Display Hw and Gw
+                fig_hw = go.Figure()
+                fig_hw.add_trace(go.Scatter(x=np.arange(len(Hw[0:60])), y=Hw, mode='lines', name='H(w)', line=dict(color='red')))
+                st.subheader('Frekuensi Respon')
+                st.subheader('h(w)')
+                st.plotly_chart(fig_hw)
+
+                fig_gw = go.Figure()
+                fig_gw.add_trace(go.Scatter(x=np.arange(len(Gw[0:60])), y=Gw, mode='lines', name='G(w)', line=dict(color='green')))
+                st.subheader('g(w)')
+                st.plotly_chart(fig_gw)
+
+                # Plot Lines Q using Plotly
+                Q = np.zeros((9, round(fs/2)+1))
+                i_list = []
+                for i in range(0, round(fs/2)+1):
+                    i_list.append(i)
+                    Q[1][i] = Gw[i]
+                    if 2*i < len(Gw):
+                        Q[2][i] = Gw[2*i]*Hw[i]
+                    if 4*i < len(Gw):
+                        Q[3][i] = Gw[4*i]*Hw[2*i]*Hw[i]
+                    if 8*i < len(Gw):
+                        Q[4][i] = Gw[8*i]*Hw[4*i]*Hw[2*i]*Hw[i]
+                    if 16*i < len(Gw):
+                        Q[5][i] = Gw[16*i]*Hw[8*i]*Hw[4*i]*Hw[2*i]*Hw[i]
+                    if 32*i < len(Gw):
+                        Q[6][i] = Gw[32*i]*Hw[16*i]*Hw[8*i]*Hw[4*i]*Hw[2*i]*Hw[i]
+                    if 64*i < len(Gw):
+                        Q[7][i] = Gw[64*i]*Hw[32*i]*Hw[16*i]*Hw[8*i]*Hw[4*i]*Hw[2*i]*Hw[i]
+                    if 128*i < len(Gw):
+                        Q[8][i] = Gw[128*i]*Hw[64*i]*Hw[32*i]*Hw[16*i]*Hw[8*i]*Hw[4*i]*Hw[2*i]*Hw[i]
+
+                fig_q = go.Figure()
+                for j in range(1, 9):
+                    fig_q.add_trace(go.Scatter(x=i_list, y=Q[j], mode='lines', name=f'Q{j}'))
+
+                fig_q.update_layout(
+                    title='Frekuensi Respon Filter',
+                    xaxis_title='Frequency (Hz)',
+                    yaxis_title='Relative Power'
+                )
+                st.subheader('Frekuensi Respon Filter')
+                st.plotly_chart(fig_q)
+
+                # Plot Q for j = 1 to 5
+                st.subheader(' Filter Wavelet')
+                for j in range(1, 6):
+                    fig_qj = plot_qj(j, dirac)
+                    st.plotly_chart(fig_qj)
+
+
+if _name_ == "_main_":
+    main()
