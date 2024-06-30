b77e2092040c73dcdda8cd87bfdee8822bacb606
afifahasnianr <afifahasniaar@gmail.com>
2024-06-30 18:15:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,28 +7,30 @@ import math
 import streamlit as st 
 
 
-########
+# Read data
 column_names = ['ECG']
-data=pd.read_csv('dataecginofix1.txt',delimiter="\t", names=column_names)
+data = pd.read_csv('dataecginofix1.txt', delimiter="\t", names=column_names)
 data["sample interval"] = np.arange(len(data))
-data["elapsed time"] = (data["sample interval"])*(1/200)
-x=data["elapsed time"]
-y=data["ECG" ] - (sum(data["ECG" ]/len(data["ECG"]))) #agar turun ke baseline
+data["elapsed time"] = data["sample interval"] * (1/200)
+x = data["elapsed time"]
+y = data["ECG"] - (sum(data["ECG"]) / len(data["ECG"]))  # Ensure the signal baseline is zero
 
-
-#DISPLAY STREAMLIT
+# Display Streamlit
 with st.sidebar:
-    selected = option_menu("TUGAS 1", ["Home", "Signal Processing","HRV Analysis","DWT"], default_index=0)
+    selected = option_menu("TUGAS 1", ["Home", "Signal Processing", "HRV Analysis", "DWT"], default_index=0)
 
 if selected == "Home":
-   st.title('Project ASN Kelompok 6')
+    st.title('Project ASN Kelompok 6')
    
-   st.subheader("Anggota kelompok")
-   new_title = '<p style="font-family:Georgia; color: black; font-size: 15px;">Afifah Hasnia Nur Rosita - 5023211007</p>'
-   st.markdown(new_title, unsafe_allow_html=True)
-   new_title = '<p style="font-family:Georgia; color: black; font-size: 15px;">Syahdifa Aisyah Qurrata Ayun - 5023211032</p>'
-   st.markdown(new_title, unsafe_allow_html=True)
-   new_title = '<p style="font-family:Georgia; color: black; font-size: 15px;">Sharfina Nabila Larasati - 5023211055</p>'
-   st.markdown(new_title, unsafe_allow_html=True)
+    st.subheader("Anggota kelompok")
+    members = [
+        "Afifah Hasnia Nur Rosita - 5023211007",
+        "Syahdifa Aisyah Qurrata Ayun - 5023211032",
+        "Sharfina Nabila Larasati - 5023211055"
+    ]
+    
+    for member in members:
+        new_title = f'<p style="font-family:Georgia; color: black; font-size: 15px;">{member}</p>'
+        st.markdown(new_title, unsafe_allow_html=True)
 
    
